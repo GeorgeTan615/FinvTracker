8fe33cd44b2aebc792171958986953bdc5bb7bad
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2023-01-18 01:28:39 +0800

    Changes to Home words

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import IndexLayout from '@/components/IndexLayout'
+import Head from 'next/head'
 
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>FinvTracker - One Stop Solution for tracking Finances.</title>
+        <meta name="description" content="Track your expenses and investments in one place, set reminders and monitor your progress over time. Take control of your finances today!" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <IndexLayout>
-        <h1 className="text-center text-7xl text-black font-bold"><span className="text-[#C36CEC]">One Stop Solution</span> for<br/>tracking Finances and Investments</h1>
+        <h1 className="text-center text-6xl text-black font-bold"><span className="text-[#C36CEC]">One Stop Solution</span> for<br/>tracking Finances and Investments</h1>
         <div className="p-5"></div>
-        <p className="text-center text-xl text-gray-500">Track your expenses and investments in one place, set reminders and 
+        <p className="text-center text-lg text-gray-500">Track your expenses and investments in one place, set reminders and 
           monitor your progress<br/>over time. Take control of your finances today!</p>
-
-
-
-
-
       </IndexLayout>
     </>
 
