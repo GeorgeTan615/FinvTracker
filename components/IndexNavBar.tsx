df39eeef470fe33893cb960402fc9d4d95b78b04
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2023-01-18 01:36:37 +0800

    Added basic authentication

--- components/IndexNavBar.tsx
+++ components/IndexNavBar.tsx
@@ -1,27 +1,40 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const IndexNavBar = () => {
-  return (
-	<nav className="flex justify-between pl-10 pr-10">
-		<Link href="/">
-			<img src="https://png.pngtree.com/png-clipart/20190903/original/pngtree-financial-icon-png-image_4420727.jpg" alt="Logo" width={50} height = {50} />
-		</Link>
-		<div className="flex justify-center gap-10 text-xl font-semibold items-center">
-			<Link href="#" legacyBehavior>
-				<a>About</a>
+	return (
+		<nav className="flex justify-between pl-10 pr-10">
+			<Link href="/">
+				<img
+					src="https://png.pngtree.com/png-clipart/20190903/original/pngtree-financial-icon-png-image_4420727.jpg"
+					alt="Logo"
+					width={50}
+					height={50}
+				/>
 			</Link>
-			<Link href="#" legacyBehavior>
-				<a>Sign Up</a>
-			</Link>
-			<Link href="#" legacyBehavior>
-				<a className="bg-[#C36CEC] py-2 px-4 text-white rounded-lg">
-					Login
-				</a>
-			</Link>
-		</div>
-	</nav>
-  )
-}
+			<div className="flex justify-center gap-10 text-lg font-semibold items-center">
+				<Link href="#" legacyBehavior>
+					<a>About</a>
+				</Link>
+				<Link href="#" legacyBehavior>
+					<a>Sign Up</a>
+				</Link>
+				<Link href="#" legacyBehavior>
+					<a
+						href={`/api/auth/signin`}
+						onClick={(e) => {
+							e.preventDefault()
+							signIn()
+						  }}
+						className="bg-[#C36CEC] py-2 px-4 text-white rounded-lg"
+					>
+						Login
+					</a>
+				</Link>
+			</div>
+		</nav>
+	);
+};
 
-export default IndexNavBar
+export default IndexNavBar;
