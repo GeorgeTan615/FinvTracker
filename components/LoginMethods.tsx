9c8d3a48459674e04ad3a1440e2496d095c9ad7f
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2023-01-18 19:46:23 +0800

    Changed logo access to using URLs

--- components/LoginMethods.tsx
+++ components/LoginMethods.tsx
@@ -11,21 +11,25 @@ interface LoginMethodsProps{
 	providers:Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null
 }
 const LoginMethods = (props:LoginMethodsProps) => {
-	// console.log(props.providers)
+	// Temp Solution. Next time can opt for Amazon s3 to store our images
+	const google = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png"
+	const github = "https://cdn-icons-png.flaticon.com/512/25/25231.png"
 	return (
 		<div className="flex flex-col justify-center items-center gap-3">
-			{props.providers && Object.values(props.providers).map((provider) => (
-				<>
-				<div className=""key={provider.name}>
-					<Button size="lg" bordered color="secondary" onClick={() => signIn(provider.id)}>
-						<Image src={`/${provider.name}.png`} alt="hello" width={30} height={30}></Image>
-						<div className="p-1"></div>
-						Sign in with {provider.name}
-					</Button>
-					
-				</div>
-				</>
-			))}
+			{props.providers && Object.values(props.providers).map((provider) => {
+				let link = provider.name ==	 'Google' ? google : github;
+				return (
+					<>
+					<div className=""key={provider.name}>
+						<Button size="lg" bordered color="secondary" onClick={() => signIn(provider.id)}>
+							<Image src={link} alt={`${provider.name} logo`} width={30} height={30}></Image>
+							<div className="p-1"></div>
+							Sign in with {provider.name}
+						</Button>
+						
+					</div>
+					</>
+			)})}
 		</div>
 	)
 }
