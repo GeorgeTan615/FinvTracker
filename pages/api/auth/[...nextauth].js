81b219a9f573324934b0cb15ed276b80587c7621
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2023-01-18 18:53:56 +0800

    remove github

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,10 +9,10 @@ export const authOptions = {
 		clientId: process.env.GOOGLE_CLIENT_ID,
 		clientSecret: process.env.GOOGLE_CLIENT_SECRET
 	  }),
-	  GitHubProvider({
-		clientId: process.env.GITHUB_CLIENT_ID,
-		clientSecret: process.env.GITHUB_CLIENT_SECRET
-	  })
+	//   GitHubProvider({
+	// 	clientId: process.env.GITHUB_CLIENT_ID,
+	// 	clientSecret: process.env.GITHUB_CLIENT_SECRET
+	//   })
     // ...add more providers here
   ],
 }
