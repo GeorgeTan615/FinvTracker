564fd7f1f8fd9be796627744b4ffc4edeb92efbc
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2023-01-20 17:30:15 +0800

    Established connection with planetscale through prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../lib/prismadb"
 
 export const authOptions = {
   // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
 	GoogleProvider({
 		clientId: process.env.GOOGLE_CLIENT_ID,
